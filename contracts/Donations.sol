5ee8554df274c3e46104775cb94bec438c257dde
William Smith <williampsmith@berkeley.edu>
2017-11-26 20:46:48 -0800

    stub for donate()

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.4.15;
 
+import './NPO.sol';
+import './utils/SafeMath.sol';
+
 contract Donations {
+
+  struct Donation {
+  	uint256 amount;
+  	string usedFor;
+  	address npo;
+  }
+
+  mapping(address => Donation[]) private donations;
+
+  function donate() payable returns (bool) {
+  }
+
 
 }
