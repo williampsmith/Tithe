1135aed0c3bfdf2c54b2c12a628f1a5479ced307
William Smith <williampsmith@berkeley.edu>
2017-11-27 18:22:45 -0800

    utilize UUIDProvider in Donations contract

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -4,24 +4,30 @@ import './NPO.sol';
 import './utils/SafeMath.sol';
 
 contract Donations {
+  address owner;
 
   struct Donation {
-    uint256 id;  // unique key for donation
+    bytes16 id;
     address donor;
+    address npo;
   	uint256 amount;
   	string usedFor;
-  	address npo;
   }
 
   // maps donor address to Donation
-  mapping(address => Donation[]) private donations;
+  mapping(address => Donation[]) public donations;
+
+  function Donations(address _owner) {
+    // should only be called once, and from Config contract
+    owner = _owner;
+  }
 
 
-  function logDonation(address donor, address npo, uint256 amount, uint256 id) {
+  function logDonation(bytes16 id, address donor, address npo, uint256 amount, string usedFor) {
     donations[npo].push(Donation(id, donor, amount, "", npo));
   }
 
-  function logWithdrawal(address npo, uint256 id, string usedFor) {
+  function logWithdrawal(address npo, bytes16 id, string usedFor) {
     donations[npo][id].usedFor = usedFor;
   }
 }
